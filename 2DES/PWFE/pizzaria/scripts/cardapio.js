6beaa28c833a3bb964f96db7156145bd2b56802e
ViniSouza01 <viniciussouza130705@gmail.com>
2023-05-19 11:21:51 -0300

    frontend: pizzaria

--- 2DES/PWFE/pizzaria/scripts/cardapio.js
+++ 2DES/PWFE/pizzaria/scripts/cardapio.js
@@ -1,19 +1,47 @@
+const body = document.querySelector('body');
 const main = document.querySelector('main');
+
 adicionarCard();
 function adicionarCard() {
-    cardapio.forEach(element => {
+    cardapio.forEach((element, index) => {
         const card = document.createElement('div');
         card.classList.add('card');
         card.innerHTML = `
-        <h1>${element.nome}</h1>
+        <h1 onclick="displays()" >${element.nome}</h1>
         <div class="info">
             <p>${element.descricao}</p>
             <footer>
-                <h3>${element.preco}</h3>
+                <h3>R$ ${element.preco}</h3>
             </footer>
     `
     main.appendChild(card);
-    console.log(card)
-    });
+    })
+}
+
+function displays() {
+    const window = document.querySelector('.window');
+    window.classList.toggle('hidden');
+    body.classList.toggle('notOverflow');
+}
+
+function showInformations(posicao) {
+    const window = document.querySelector('.window');
+    const existingInfo = document.getElementById('informacoes');
     
+    // Remover informações existentes, se houver
+    if (existingInfo) {
+        window.removeChild(existingInfo);
+    }
+    let informacoes = document.createElement('div');
+
+    // remover o conteúdo anterior da janela
+    informacoes.innerHTML = '';
+
+    informacoes.innerHTML = `
+        <p>${cardapio[posicao].id}</p>
+        <p>${cardapio[posicao].nome}</p>
+        <p>${cardapio[posicao].descricao}</p>
+        <p>${cardapio[posicao].preco}</p>    
+    `;
+    window.appendChild(informacoes);
 }
