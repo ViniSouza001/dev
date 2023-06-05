6be8a470d889c60fd514bf271db566cabe29f564
ViniSouza01 <viniciussouza130705@gmail.com>
2023-06-05 08:26:59 -0300

    bcd: aula12

--- 2DES/BCD/aula12/consultas.sql
+++ 2DES/BCD/aula12/consultas.sql
@@ -7,4 +7,4 @@ SELECT * FROM venda WHERE id_cliente = 4;
 -- 3. Crie um relatorio que mostre todas as vendas com os seguintes campos: 
 -- id da venda, nome do vendedor, nome do cliente, data e valor em ordem de data decrescente; 
 -- Salve com o nome de vw_vendas_gerais; OBS: vendas em que id_vendedor ou id_cliente 
--- serão nulos não devem aparecer;
+-- sejão nulos não devem aparecer;
