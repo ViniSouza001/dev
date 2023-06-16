6310d66a004b4c5928fcae873b2e68232a5cf4ab
ViniSouza01 <viniciussouza130705@gmail.com>
2023-06-16 08:07:57 -0300

    quiz

--- 2DES/INDMO/quiz/sources/screens/quiz.screen.js
+++ 2DES/INDMO/quiz/sources/screens/quiz.screen.js
@@ -17,10 +17,13 @@ export default function Quiz() {
     return (
         <View>
             <Image source={require('../../assets/ceu.jfif')} style={styles.img} />
-            <Text>Testando...</Text>
-            <TouchableOpacity onPress={() => {mudarPergunta()}}>
-                <Text style={styles.txt}>Apenas um show</Text>
-            </TouchableOpacity>
+            <View style={styles.div}>
+                    <Text style={styles.title}>Pergunta {contador + 1}</Text>
+                    <Text style={styles.questions}>{perguntas[contador].text}</Text>
+                <TouchableOpacity onPress={() => {mudarPergunta()}}>
+                </TouchableOpacity>
+
+            </View>
         </View>
     )
 }
@@ -29,5 +32,22 @@ const styles = StyleSheet.create({
     img: {
         width: '100vw',
         height: '100vh',
+    },
+    div: {
+        width: '80vw',
+        height: '100vh',
+        position: 'absolute',
+        flexDirection: 'column',
+        alignSelf: 'center',
+        textAlign:'center',
+        justifyContent: 'space-evenly'
+    },
+    questions: {
+        color: "#fff",
+        fontSize: 20
+    },
+    title: {
+        color: '#fff',
+        fontSize: 25
     }
 })
