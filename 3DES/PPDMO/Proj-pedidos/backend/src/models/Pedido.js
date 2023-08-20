6405e456ef9b3218bc8aa715935e280a1c549140
ViniSouza001 <viniciussouza130705@gmail.com>
2023-08-20 13:49:50 -0300

    model pedidos

--- 3DES/PPDMO/Proj-pedidos/backend/src/models/Pedido.js
+++ 3DES/PPDMO/Proj-pedidos/backend/src/models/Pedido.js
@@ -31,19 +31,19 @@ PedidoSchema.pre('save', async function (next) {
 
 const Pedido = mongoose.model('pedidos', PedidoSchema)
 
-// var novoPedido = new Pedido({
-//     "idCliente": '_id: ObjectId("64cb915bcf75126f99c5105d")',
-//     "valorPedido": 300,
-//     "paraEntrega": true,
-//     "cep": "13920-000",
-//     'endereco': "alameda Cambuci",
-//     "valorEntrega": "210",
-//     "itens": ["Coca cola 300ml", "Frango a passarinho", "Mortadela defumada", "Marmita tamanho GG"]
+    // var novoPedido = new Pedido({
+    //     "idCliente": '_id: ObjectId("64cb915bcf75126f99c5105d")',
+    //     "valorPedido": 300,
+    //     "paraEntrega": true,
+    //     "cep": "13920-000",
+    //     'endereco': "alameda Cambuci",
+    //     "valorEntrega": "210",
+    //     "itens": ["Coca cola 300ml", "Frango a passarinho", "Mortadela defumada", "Marmita tamanho GG"]
 
-// })
+    // })
 
-// novoPedido.save().then(() => console.log('pedido salvo com sucesso'))
-//     .catch(err => console.log('erro ao salvar o pedido: ' + err))
+    // novoPedido.save().then(() => console.log('pedido salvo com sucesso'))
+    //     .catch(err => console.log('erro ao salvar o pedido: ' + err))
 
 
 
