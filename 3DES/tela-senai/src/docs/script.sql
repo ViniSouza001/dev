660a76fba7c9a9da7067d6138421863f6e5177ad
ViniSouza001 <viniciussouza130705@gmail.com>
2023-09-05 11:17:56 -0300

    tela-senai

--- 3DES/tela-senai/src/docs/script.sql
+++ 3DES/tela-senai/src/docs/script.sql
@@ -15,6 +15,7 @@ create table livros (
 
 insert into livros values 
 (null, "Clean Code", "Computer Science", "Robert Cecil Martin", "2000", "It teaches how to write high-quality, understandable, and maintainable code. With practical examples, the book covers techniques to improve code readability and structure, ranging from choosing appropriate variable names to organizing functions and classes.", 10.00),
-(null, "Fundamentos Matemáticos para a ciência da computação", "Computer Science", "Judith L. Gersting", "2010", "Este livro irá te auxiliar nas matérias de eletroeletrônica e em todos os seus componentes", 15.00),
-(null, "The Alchemist", "Fiction", "2000", "O livro 'Clean Code: A Handbook of Agile Software Craftsmanship' de Robert C. Martin é uma leitura fundamental para programadores. Ele ensina como escrever código de qualidade, fácil de entender e manter. Com exemplos práticos, o autor aborda técnicas para melhorar a legibilidade e estrutura do código, abrangendo desde a escolha de nomes adequados para variáveis até a organização de funções e classes. 'Clean Code' é essencial para desenvolvedores que desejam aprimorar suas habilidades e contribuir para projetos de software mais profissionais e eficazes.", 10.00),
-(null, "Electro-Eletrônica", "Eletroeletrônica", "2010", "Este livro irá te auxiliar nas matérias de eletroeletrônica e em todos os seus componentes", 15.00);
+(null, "Fundamentos Matemáticos para a ciência da computação", "Computer Science", "Judith L. Gersting", "2010", "", 15.00),
+(null, "The Alchemist", "Fiction", "1988", "Follow the journey of a young shepherd named Santiago as he sets out to discover his personal legend and find his true purpose in life", 30.00),
+(null, "Sapiens: A Brief History of Humankind", "Non-Fiction", "2011", "Explore the fascinating history of Hommo sapiens and how oue species has envolved and shaped the world we live in today", 60.00),
+(null, "The Harry Potter Series", "Fantasy", "")
